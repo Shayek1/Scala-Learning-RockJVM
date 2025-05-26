bc5b50b4b0fac84dd465e6dbbf8ae92b3e90ac97
Shayek Ahmed <shayek.a@outlook.com>
2025-05-27 00:49:26 +0100

    Begin Pattern Matching Lesson

--- src/main/scala/com/rockthejvm/part4power/PatternMatching.scala
+++ src/main/scala/com/rockthejvm/part4power/PatternMatching.scala
@@ -8,6 +8,13 @@ object PatternMatching {
   val random = new Random()
   val aValue = random.nextInt(100)
 
+  val description = aValue match{
+    case 1 => "first"
+    case 2 => "second"
+    case 3 => "third"
+    case _ => "some other thing"
+  }
+
 
 
   def main(args: Array[String]): Unit = {
