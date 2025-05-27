0a2f1f3f22ad577c281cf994da374f69fe5fbb7f
Shayek Ahmed <shayek.a@outlook.com>
2025-05-27 09:25:26 +0100

    Complete pattern match lesson

--- src/main/scala/com/rockthejvm/part4power/PatternMatching.scala
+++ src/main/scala/com/rockthejvm/part4power/PatternMatching.scala
@@ -12,9 +12,24 @@ object PatternMatching {
     case 1 => "first"
     case 2 => "second"
     case 3 => "third"
-    case _ => "some other thing"
+    case _ => s"some other value $aValue "
+  } //expression that returns a string
+//if there's no match, u get match error
+
+  //can decompose values - pattern matching only on cases classes
+  case class Person(name: String, age:Int)
+  val al = Person("Al", 65)
+
+  //patterns match in sequence
+  val greet = al match{
+    case Person(n, a) if a < 18 => s"hi i'm $n and i'm $a years old" //if statement here is a guard
+    case Person(n, a) => s"hello my name is $n"
+    case _ => "who am i?"
   }
 
+  //on a sealed hierarchy
+  sealed class Animal
+  case class Dog(breed:String) extends Animal
 
 
   def main(args: Array[String]): Unit = {
