d1182fb03d6b9c359c82cf20e662f58aa0c7213c
Shayek Ahmed <shayek.a@outlook.com>
2025-05-29 20:01:20 +0100

    Complete pattern match lesson exercises

--- src/main/scala/com/rockthejvm/part4power/PatternMatching.scala
+++ src/main/scala/com/rockthejvm/part4power/PatternMatching.scala
@@ -30,9 +30,47 @@ object PatternMatching {
   //on a sealed hierarchy
   sealed class Animal
   case class Dog(breed:String) extends Animal
+  case class Cat(meowType: String) extends Animal
+  
+  val anAnimal: Animal = Dog("German Shepherd")
+  val animalChecker = anAnimal match{
+    case Dog(aBreed) => "This is a dog"
+    case Cat(meow) => "This is a Cat"
+  }
+
+  /**
+   * exercise
+   * show(Sum(Number(2), Number(3))) = "2 + 3"
+   * show(Sum(Sum(Number(2), Number(3)), Number(4))) = "2 + 3 + 4"
+   * show(Prod(Sum(Number(2), Number(3)), Number(4))) = "(2 + 3) * 4"
+   * show(Sum(Prod(Number(2), Number(3)), Number(4))) = "2 * 3 + 4
+   *
+   */
+
+  sealed trait Expr
+  case class Number(n: Int) extends Expr
+  case class Sum(e1: Expr, e2: Expr) extends Expr
+  case class Prod(e1: Expr, e2: Expr) extends Expr
+
+  def show(expr:Expr): String = expr match{ //compiler first checks if there it is just a number and then a Sum class.
+    case Number(n) => s"$n"
+    case Sum(a, b) => show(a) + " + " + show(b)
+    case Prod(a, b) => {
+      def prodPaths(x : Expr) = x match{ // this shows the different pattern cases that can happen after there is a prod class
+        case Prod(a,b) => show(x) //first checks if it is in a prod class
+        case Number(n) => show(x) //then if just number
+        case Sum(a, b) => s"(${show(x)})" //if the numbers are in a sum
+      }
+      prodPaths(a) + " * " + prodPaths(b)
+    }
+  }
 
 
   def main(args: Array[String]): Unit = {
+    println(show(Sum(Number(2), Number(3))))
+    println(show(Sum(Sum(Number(2), Number(3)), Number(4))))
+    println(show(Prod(Sum(Number(2), Number(3)), Number(4))))
+    println(show(Sum(Prod(Number(2), Number(3)), Number(4))))
 
 
 
