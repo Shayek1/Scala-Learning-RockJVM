493eeade47a39f4c79c8f53b18436ecf585e579f
Shayek Ahmed <shayek.a@outlook.com>
2025-05-29 20:33:12 +0100

    Complete constants section of All the Pattern lesson

--- src/main/scala/com/rockthejvm/part4power/AllThePatterns.scala
+++ src/main/scala/com/rockthejvm/part4power/AllThePatterns.scala
@@ -1,6 +1,22 @@
 package com.rockthejvm.part4power
 
+object MySingleton
+
 object AllThePatterns {
+
+  //All the patterns we can use:
+
+  //Constants
+  val aVal: Any = "Scala" //compiler knows what type the code is
+  val consts = aVal match{
+    case 32 => "number"
+    case "Scala" => "THE Scala"
+    case true => "truth"
+    case MySingleton => "a singleton object"
+  }
+
+  //Matching anything
+
 
 
   def main(args: Array[String]): Unit = {
