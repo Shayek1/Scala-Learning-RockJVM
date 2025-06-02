dcd13f48631e73a8ca68fbc520d7e429e01760ce
Shayek Ahmed <shayek.a@outlook.com>
2025-06-02 03:32:43 +0100

    Finish Imperative Programming Lesson

--- src/main/scala/com/rockthejvm/part4power/ImperativeProgramming.scala
+++ src/main/scala/com/rockthejvm/part4power/ImperativeProgramming.scala
@@ -2,4 +2,32 @@ package com.rockthejvm.part4power
 
 object ImperativeProgramming {
 
+  val meaningOfLife = 23 //val - cant be changed after declared
+
+  var aVar = 55 // var - can be changed after declared
+
+  //there is no ++ , must use +=
+
+  aVar += 10 // means aVar = aVar + 10
+
+  //loops
+  def loopTest(): Unit = {
+    var i = 0
+    while (i < 10) {
+      println(s"Counter at $i")
+      i += 1
+    }
+  }
+
+   //imperative programming makes debugging hard
+   // should not use for no reason, scala is not for that purpose
+  /* where I P is useful
+  when using performance critical applications
+  interactions with java libraries
+   */
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
 }
